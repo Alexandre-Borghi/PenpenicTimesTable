7b06a8f189086844ed3462b14301ee6a586dfaf2
Alexandre-Borghi <borghi.alexandre.12@gmail.com>
2022-06-11 11:18:31 +0200

    Change Crate Name
    Change crate name to snake case

--- docs/penpenic_times_table-e720b2865b490421.js
+++ docs/penpenic_times_table-e720b2865b490421.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('PenpenicTimesTable-2cbdbf7ea6d7732_bg.wasm', import.meta.url);
+        input = new URL('penpenic_times_table-e720b2865b490421_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
