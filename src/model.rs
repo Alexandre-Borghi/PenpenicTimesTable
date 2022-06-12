4cb4fcc84c6a826741dc1bff68ae92fed554e77e
Alexandre-Borghi <borghi.alexandre.12@gmail.com>
2022-06-12 23:07:26 +0200

    Time::new Tests
    Write tests for the Time::new function

--- src/model.rs
+++ src/model.rs
@@ -32,10 +32,54 @@ struct Time {
 }
 
 impl Time {
-    fn from_time_input_value(value: String) -> Option<Self> {
+    fn new(hours: u8, minutes: u8) -> Result<Self, TimesTableError> {
+        if hours > 23 || minutes > 59 {
+            Err(TimesTableError::InvalidTime)
+        } else {
+            Ok(Self { hours, minutes })
+        }
+    }
+
+    fn from_time_input_value(value: &str) -> Option<Self> {
         let mut splits = value.split(':');
         let hours = splits.next()?.parse::<u8>().ok()?;
         let minutes = splits.next()?.parse::<u8>().ok()?;
         Some(Self { hours, minutes })
+    }
+}
+
+#[derive(Debug)]
+enum TimesTableError {
+    InvalidTime,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_works() -> Result<(), TimesTableError> {
+        new_with_valid_input(10, 20)?;
+        new_with_valid_input(5, 30)?;
+        new_with_valid_input(13, 54)?;
+        new_with_valid_input(19, 27)?;
+        Ok(())
+    }
+
+    fn new_with_valid_input(hours: u8, minutes: u8) -> Result<(), TimesTableError> {
+        let t = Time::new(hours, minutes)?;
+        assert_eq!(t.hours, hours);
+        assert_eq!(t.minutes, minutes);
+        Ok(())
+    }
+
+    #[test]
+    fn parses_hh_mm() -> Result<(), TimesTableError> {
+        assert_eq!(
+            Time::from_time_input_value("05:30").unwrap(),
+            Time::new(5, 30)?
+        );
+
+        Ok(())
     }
 }
