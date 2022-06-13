2d180694a404884b80e8dab4950051fd78d46165
Alexandre-Borghi <borghi.alexandre.12@gmail.com>
2022-06-13 15:52:38 +0200

    Add Small Description Text to TimesTableRowRange
    Add small text to indicate that inputs of TimesTableRowRange correspond
    to the start and the end of the period

--- src/components/times_table_row_range.rs
+++ src/components/times_table_row_range.rs
@@ -10,9 +10,11 @@ pub fn times_table_row_range() -> Html {
         <>
             <div class={classes!("col")}>
                 <input ref={start_input_ref} type="time" class={classes!("form-control")} />
+                <div class={classes!("form-text")}>{ "Début" }</div>
             </div>
             <div class={classes!("col")}>
                 <input ref={end_input_ref} type="time" class={classes!("form-control")} />
+                <div class={classes!("form-text")}>{ "Fin" }</div>
             </div>
         </>
     }
